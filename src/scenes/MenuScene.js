ff18801b9a847c4f6f2cfd34cccac45169bbed0d
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-07-29 16:59:00 +0530

    Menu items added

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -6,13 +6,23 @@ import BaseScene from "./BaseScene";
 class MenuScene extends BaseScene{
   constructor(config) {
     super("MenuScene",config);
+
+    this.menu = [
+        { scene: "PlayScene", text: "Play" },
+        { scene: "ScoreScene", text: "Score" },
+        { scene: null, text: "Exit" },
+        ];
   
   }
 
   create() {
     super.create();
-    this.scene.start("PlayScene");
+
+    this.createMenu(this.menu);
+  
   }
+
+   
 }
 
 export default MenuScene;
