4f5e9043e02c5b728d951e063349b90a583e5488
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-07-29 17:37:59 +0530

    setup menu events

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -3,26 +3,35 @@ import Phaser from "phaser";
 import BaseScene from "./BaseScene";
 
 
-class MenuScene extends BaseScene{
+class MenuScene extends BaseScene {
   constructor(config) {
-    super("MenuScene",config);
+    super("MenuScene", config);
 
     this.menu = [
-        { scene: "PlayScene", text: "Play" },
-        { scene: "ScoreScene", text: "Score" },
-        { scene: null, text: "Exit" },
-        ];
-  
+      { scene: "PlayScene", text: "Play" },
+      { scene: "ScoreScene", text: "Score" },
+      { scene: null, text: "Exit" },
+    ];
   }
 
   create() {
     super.create();
 
-    this.createMenu(this.menu);
-  
+    this.createMenu(this.menu, this.setupMenuEvents.bind(this));
   }
 
-   
+  setupMenuEvents(menuItem) {
+    const textGO = menuItem.textGO;
+    textGO.setInteractive();
+
+    textGO.on("pointerover", () => {
+      textGO.setStyle({ fill: "#ff0" });
+    });
+
+    textGO.on("pointerout", () => {
+      textGO.setStyle({ fill: "#fff" });
+    });
+  }
 }
 
 export default MenuScene;
