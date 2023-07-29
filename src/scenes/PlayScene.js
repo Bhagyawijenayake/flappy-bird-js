8afb1708c15e2c461d1c13fef7ac963fabed662c
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2023-07-29 12:32:30 +0530

    World bound colliders added

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -46,14 +46,21 @@ class PlayScene extends Phaser.Scene {
       .sprite(this.config.startPosition.x, this.config.startPosition.y, "bird")
       .setOrigin(0);
     this.bird.body.gravity.y = 400;
+    this.bird.setCollideWorldBounds(true);
   }
 
   createPipes() {
     this.pipes = this.physics.add.group();
 
     for (let i = 0; i < PIPES_TO_RENDER; i++) {
-      const upperPipe = this.pipes.create(0, 0, "pipe").setOrigin(0, 1);
-      const lowerPipe = this.pipes.create(0, 0, "pipe").setOrigin(0, 0);
+      const upperPipe = this.pipes
+        .create(0, 0, "pipe")
+        .setImmovable(true)
+        .setOrigin(0, 1);
+      const lowerPipe = this.pipes
+        .create(0, 0, "pipe")
+        .setImmovable(true)
+        .setOrigin(0, 0);
 
       this.placePipe(upperPipe, lowerPipe);
     }
@@ -73,8 +80,8 @@ class PlayScene extends Phaser.Scene {
 
   checkGameStatus() {
     if (
-      this.bird.y > this.config.height ||
-      this.bird.y < 0 - this.bird.height
+      this.bird.getBounds().bottom >= this.config.height ||
+      this.bird.y <= 0 
     ) {
       this.gameOver();
     }
@@ -124,9 +131,12 @@ class PlayScene extends Phaser.Scene {
   }
 
   gameOver() {
-    this.bird.x = this.config.startPosition.x;
-    this.bird.y = this.config.startPosition.y;
-    this.bird.body.velocity.y = 0;
+    // this.bird.x = this.config.startPosition.x;
+    // this.bird.y = this.config.startPosition.y;
+    // this.bird.body.velocity.y = 0;
+
+    this.physics.pause();
+    this.bird.setTint(0xEE4824);
   }
 
   flap() {
